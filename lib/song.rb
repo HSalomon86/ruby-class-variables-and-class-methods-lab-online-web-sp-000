196b191dac4bd5fe1b753e837a47c1b46f0f0429
Hendrick Salomon <hsalomon86@gmail.com>
2020-02-05 02:59:19 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0 
+  @@genres = []
+  def initialize(name, artist, genre)
+    @genre = genre
+    @artist = artist
+    @name = name
+
+    @@count += 1
+
+      if !@@genres.include?(genre)
+        @@genres << genre
+      end
+    
+  end
+
+  def self.genres
+    @@genres
+  end
+
+  def self.count
+    @@count
+  end
+
+end
